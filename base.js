a3f2d37f3cfe591db4762e7ab784edf2bba53ac3
joaokonno <jvkonno@hotmail.com>
2025-11-11 22:12:25 -0300

    feat(ui): implement displayBooks() for rendering book data
    - Implement displayBooks() to render each book's title, author, and
    page count in a table
    - Add temporary test data for verifying table rendering

--- base.js
+++ base.js
@@ -11,3 +11,35 @@ function addBookToLibrary(title, author, pages){
     const book = new Book(title, author, pages);
     myLibrary.push(book);
 }
+
+function displayBooks(){
+    const table = document.createElement('table'); // create table
+    document.body.appendChild(table); // append table to the document
+    const book = {title: 'test', author: 'test', pages: 1}; // create an instance of Book for getting key
+    const keys = Object.keys(book); // get the Book properties
+    const header_row = document.createElement('tr'); // create the header row
+    table.appendChild(header_row); // append header row to the table
+
+    for (const key of keys){ // loop through the keys to populate the header row
+        const td = document.createElement('td');
+        td.innerHTML = key
+        header_row.appendChild(td);
+    }
+
+    for (const book of myLibrary){ // create a row for every book
+        const tr = document.createElement('tr');
+        table.appendChild(tr);
+        for (const key of keys){ // populate each row with book data
+            const td = document.createElement('td');
+            tr.append(td);
+            td.innerHTML = book[key]
+        }
+    }
+   
+}
+
+// some test data
+addBookToLibrary('black holes', 'stephen hawking', 310);
+addBookToLibrary('understanding analysis', 'abbott', 200);
+addBookToLibrary('introduction to electrodynamics', 'griffiths', 400);
+displayBooks();
